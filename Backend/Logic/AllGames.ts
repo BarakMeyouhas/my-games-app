ad2da1fb190fe5b8b053c0cb49e9c9517a0a7085
Barak1998 <barakm25@gmail.com>
2024-01-26 09:09:12 +0200

    added search game by name route and logic

--- Backend/Logic/AllGames.ts
+++ Backend/Logic/AllGames.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 
-const gamesURL = "http://www.freetogame.com/api/games";
-const gameByIdURL = "https://www.freetogame.com/api/game?id=";
+const gamesURL =
+  "https://api.rawg.io/api/games?key=c1cff7ce8544427797e07592e1e53079&dates=2018-01-01,2018-12-31&ordering=-added&page=1";
+const gameByIdURL = "https://api.rawg.io/api/games";
+const searchGamesURL = "https://api.rawg.io/api/games?search=";
 
 const allGames = async () => {
   try {
     const response = await axios.get(gamesURL);
+    console.log(response.data);
     return response.data;
   } catch (error) {
     return null;
@@ -14,13 +17,24 @@ const allGames = async () => {
 
 const getGameById = async (gameId: string) => {
   try {
-    const response = await axios.get(`${gameByIdURL}${gameId}`);
+    const response = await axios.get(`${gameByIdURL}/${gameId}`);
     console.log(gameId);
     return response.data;
-    
   } catch (error) {
     return null;
   }
 };
 
-export { allGames, getGameById };
+const searchGames = async (searchTerm: string) => {
+  try {
+    const response = await axios.get(
+      `${searchGamesURL}${searchTerm}&key=c1cff7ce8544427797e07592e1e53079`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    return null;
+  }
+};
+
+export { allGames, getGameById, searchGames };
--- Backend/Routes/GameRouter.ts
+++ Backend/Routes/GameRouter.ts
@@ -1,12 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
-import { allGames, getGameById } from "../Logic/AllGames";
+import { allGames, getGameById, searchGames } from "../Logic/AllGames";
 const gameRouter = express.Router();
 
 gameRouter.get(
   "/allGames",
   async (request: Request, response: Response, next: NextFunction) => {
     const data = await allGames();
+    console.log(data);
     response.status(200).json(data);
+    console.log(data);
   }
 );
 
@@ -19,12 +21,36 @@ gameRouter.get(
       const game = await getGameById(gameId);
 
       if (!game) {
-        // Game not found
         response.status(404).json({ message: "Game not found" });
         return;
       }
 
       response.status(200).json(game);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+gameRouter.get(
+  "/searchGames/:searchTerm",
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const searchTerm = request.params.searchTerm;
+
+      if (!searchTerm) {
+        response.status(400).json({ message: "Search term is required" });
+        return;
+      }
+
+      const games = await searchGames(searchTerm);
+
+      if (!games) {
+        response.status(500).json({ message: "Internal server error" });
+        return;
+      }
+
+      response.status(200).json(games);
     } catch (error) {
       next(error);
     }
